400b67b601425dc81e18a0a779f475d77127cfaf
박건우 <woorl201362@gmail.com>
2024-11-29 10:20:02 +0900

    1. OCR_TodoList_Components 초안
    1-1. conponents 폴더 => TodoList 폴더 생성
    1-2. 목록이 적힌 TodoList.jsx 컴퍼넌트 생성

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import TodoList from './component/Todolist/TodoList'
+
+function App() {
+  return (
+    <div>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
